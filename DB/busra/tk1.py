2c89196014c7dffac477685676028f3d1020da1f
busrakeles15 <60065964+busrakeles15@users.noreply.github.com>
2020-02-23 14:18:47 +0200

    23022020

--- DB/busra/tk1.py
+++ DB/busra/tk1.py
@@ -1,3 +1,41 @@
-import tkinter as tk
-pencere = tk.Tk()
+from tkinter import *
+from tkinter import messagebox,filedialog
+pencere = Tk()
+#-------------------
+etiket = Label(text="Adı:")
+etiket.grid(column=0,row=0)
+#--------------------
+textBox = Entry()
+textBox.grid(column=1,row=0)
+#-------------------
+def tiklandi():
+    print(textBox.get())
+    print(rdb.getboolean(varRdb))
+    print(varRdb)
+dugme = Button(text="Aktarım",command=tiklandi)
+dugme.grid(column=2,row=0)
+#--------------------
+varRdb = BooleanVar()
+varRdb = False
+rdb = Radiobutton(text="deneme",value=varRdb,command=tiklandi)
+rdb.grid(column=3,row=0)
+#--------------------
+def mesaj():
+    mesaj="Merhaba Büşra"
+    #mesajBox= messagebox.showinfo("Yazılım",mesaj)
+    mesaj = messagebox.askyesno("Yazılım","Hoşgeldiniz dosyayı açmak istiyor musunuz? ")
+    if mesaj:
+        dosya= filedialog.askopenfile(initialdir="/",
+        title = "Dosya Aç",
+        filetypes=(("Resim Dosyaları",".jpeg"),("Bütün Dosyalar","*.*")))
+menu= Menu(pencere)
+pencere.config(menu=menu)
+dosyaMenu=Menu(menu)
+menu.add_cascade(label="Dosya", menu=dosyaMenu)
+dosyaMenu.add_command(label="Yeni",command=mesaj)
+dosyaMenu.add_command(label="Çıkış",command=pencere.quit)
+
+
+pencere.geometry("300x300+100+100")
+pencere.title("Büşra")
 pencere.mainloop()
